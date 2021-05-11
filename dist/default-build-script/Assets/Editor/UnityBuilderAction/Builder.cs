9eeaa1201015e116bf77005217f9b3b17d2c57d2
longlegsstudio <83555014+longlegsstudio@users.noreply.github.com>
2021-05-11 20:36:30 +0300

    Update Builder.cs

--- dist/default-build-script/Assets/Editor/UnityBuilderAction/Builder.cs
+++ dist/default-build-script/Assets/Editor/UnityBuilderAction/Builder.cs
@@ -20,7 +20,7 @@ public class Builder {
 
   static void PerformAndroidBuild() {
     UnityEditor.EditorUserBuildSettings.exportAsGoogleAndroidProject = true;
-    GenericBuild(SCENES, ./, UnityEditor.BuildTarget.Android, UnityEditor.BuildOptions.None);
+    GenericBuild(SCENES, "./", UnityEditor.BuildTarget.Android, UnityEditor.BuildOptions.None);
     TARGET_DIR + "/android/"
   }
 
