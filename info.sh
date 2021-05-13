8a063834e929d28b3a325c057a4b39992c909dfa
longlegsstudio <83555014+longlegsstudio@users.noreply.github.com>
2021-05-14 01:59:08 +0300

    Create info.sh

---
+++ info.sh
@@ -0,0 +1,6 @@
+ls -aR "./test-project/android"
+echo "========================1" 
+ls -aR "opt/unity/Editor/Data/PlaybackEngines/AndroidPlayer/NDK" 
+echo "========================2"
+ls -aR "opt/unity/Editor/Data/PlaybackEngines/AndroidPlayer/SDK" 
+echo "========================3"
