192bfab7ea11c476774273499713e4dc31506546
longlegsstudio <83555014+longlegsstudio@users.noreply.github.com>
2021-05-14 02:33:16 +0300

    Create info2.sh

---
+++ info2.sh
@@ -0,0 +1,4 @@
+ls -aR "opt/unity/Editor/Data/PlaybackEngines/AndroidPlayer/NDK" 
+echo "========================2"
+ls -aR "opt/unity/Editor/Data/PlaybackEngines/AndroidPlayer/SDK" 
+echo "========================3"
